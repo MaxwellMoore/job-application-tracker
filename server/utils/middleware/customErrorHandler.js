222bf31f44a414782a81ad5273d17e372d6af891
Maxwell <maxwellmoore.work@gmail.com>
2024-04-24 08:50:05 -0600

    Addition to CustomerErrorHandler

--- server/utils/middleware/customErrorHandler.js
+++ server/utils/middleware/customErrorHandler.js
@@ -1,4 +1,8 @@
 const customErrorHandler = (err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    console.error(err.stack);
+    res.status(401).json({ error: "Not authenticated" });
+  }
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 };
